c0307f4c215fc95d994a3a03de26c5198d022c81
Antoine LORAIN <antoine.lorain01@gmail.com>
2025-01-23 14:41:12 +0100

    Add files via upload

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import freenect
 import cv2
 import frame_convert2
+from src.get_config_params import *
 
 def get_depth():
     return frame_convert2.pretty_depth_cv(freenect.sync_get_depth()[0])
 
 while 1:
-    cv2.imshow('Depth', get_depth())
+    cv2.imshow(get_window_title(), get_depth())
     if cv2.waitKey(10) == 27:
         break
--- src/test.py
+++ src/test.py
@@ -1,25 +1,3 @@
-import freenect
-import cv2
-import numpy as np
-import frame_convert2
-import json
+from get_config_params import *
 
-def get_depth():
-    return frame_convert2.pretty_depth_cv(freenect.sync_get_depth()[0])
-
-print(len(get_depth()[0].tolist()))
-old_list = get_depth().tolist()
-newList = []
-for y in range(480):
-    newList.append([])
-    for x in range(640):
-        newList[y].append((old_list[y][x], old_list[y][x], old_list[y][x]))
-
-file = open("data.txt", "w")
-file.write(str(newList))
-file.close()
-npy = np.array(newList, dtype=np.uint8)
-cv2.imshow('Temp Picture', npy)
-while 1:
-    if cv2.waitKey(10) ==27:
-        break
+print(get_colors())
