ac186526f556c9217cc25fdb279c47fa414431b5
strykkk <119400087+strykkk@users.noreply.github.com>
2025-02-08 20:26:15 +0100

    com
    ajout de comentair sur possibilitée d'opti

--- src/get_np_array.py
+++ src/get_np_array.py
@@ -11,22 +11,23 @@ imageGrey = imageRGB.convert("L")
 image_data = np.asarray( imageGrey )
 old_list = image_data.tolist()
 newList = []
-for y in range(480):
-    newList.append([])
-    for x in range(640):
+
+for y in range(480):                # si deux ligne identique on peut peut etre opti 
+    newList.append([])              #creation direct d'une liste de 480 []
+    for x in range(640):            #traitement de plusieur pixel a la fois ?
 
         if last_color[0] == old_list[y][x]:
             newList[y].append(last_color[1])
         else:                 
             jsp = False
             for loop in test1: 
-                if old_list[y][x] >= int(loop[0][0]) and old_list[y][x] <= int(loop[0][1]):
+                if old_list[y][x] >= int(loop[0][0]) and old_list[y][x] <= int(loop[0][1]):   # trop de test peut etre ?
                     newList[y].append((loop[1]))
                     last_color = [old_list[y][x],loop[1]]
                     jsp = True
                     break
-            if jsp != True:
-                newList[y].append((255,0,0))
+            if jsp != True:                     #possibiltée de simplification 
+                newList[y].append((255,0,0))    #
    
 
             
